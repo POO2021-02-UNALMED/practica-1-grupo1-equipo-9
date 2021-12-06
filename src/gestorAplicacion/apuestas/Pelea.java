1d7f59febbab9097b8ac9f910795e15edce4e1f5
Alejandro Salazar Mejía <alejosalazarmejia.2000@gmail.com>
2021-12-06 14:36:47 -0500

    Update Pelea.java

--- src/gestorAplicacion/apuestas/Pelea.java
+++ src/gestorAplicacion/apuestas/Pelea.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gestorAplicacion.apuestas;
+import java.util.ArrayList;
+import java.util.Hashtable;
 
-/**
- *
- * @author camilo
- */
+import gestorAplicacion.carcel.Celda;
+import gestorAplicacion.carcel.Prisionero;
+
 public class Pelea {
+    private int codigo;
+    private gestorAplicacion.carcel.genero genero;
+    private Prisionero luchador1;
+    private Prisionero luchador2;
+    private String armaLuchador1;
+    private String armaLuchador2;
+    private Prisionero ganador;
+    private Apuesta apuesta;
     
+    private static Hashtable<Integer, Pelea> peleas;
+    
+    public
+    void setGanador(Prisionero prisionero) {
+//    	TODO 
+    }
+    
+    public
+    void setLuchadores(Prisionero luch1, Prisionero luch2) {
+//    	TODO
+    }
+    
+    public 
+    Object battleRoyale(ArrayList<Celda> celdas){
+		/*
+		 * Devuelve un arraylist de strings, donde cada string es un comentario tipo
+		 * "prisionero 1 a derrotado a prisionero 2", etc también debe devolver el
+		 * prisionero ganador
+		 */
+//		TODO
+    	ArrayList<String> combates = new ArrayList<String>();
+    	Prisionero ganador = null;
+    	Object[] resultado = {ganador, combates};
+    	return resultado;
+    }
 }
