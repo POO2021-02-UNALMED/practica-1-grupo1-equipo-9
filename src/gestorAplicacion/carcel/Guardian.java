28c1ec3605f80dd82167a450587fb36156cee774
Alejandro Salazar Mejía <alejosalazarmejia.2000@gmail.com>
2021-12-06 15:09:05 -0500

    Avnce en Pelea y Guardian

--- src/gestorAplicacion/carcel/Guardian.java
+++ src/gestorAplicacion/carcel/Guardian.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gestorAplicacion.carcel;
 
-/**
- *
- * @author camilo
- */
-public class Guardian {
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import gestorAplicacion.apuestas.Apostador;
+
+public class Guardian extends Apostador{
+    private int salario;
+    private ArrayList<Object[]> traslados = new ArrayList<>();
+    private Hashtable<Integer, Celda> celdas = new Hashtable<>();
     
+    private static Hashtable<Integer, Guardian> guardianes = new Hashtable<>();
+    
+    public
+    void trasladarPrisionero(Prisionero prisionero, Celda celda) {
+		/*
+		 * Utiliza el método agregarTraslado y le pasa un Object[3] donde
+		 * el primero elemento es la Celda de origen, el segundo es el prisionero
+		 * trasladado y la tercera es la celda destino
+		 */
+    }
+    
+    private
+    void agregarTraslado(Object[] objetos) {
+//    	agrega la información del traslado al los 'traslados'(atributo) que ha hecho this Apostador 
+    }
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
