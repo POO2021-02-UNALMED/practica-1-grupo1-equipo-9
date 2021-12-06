53bfba57c431c4671ca05764ec34d59f06621c41
Valentina Gomez Valencia <begomezv@unal.edu.co>
2021-12-06 17:27:31 -0500

    Update Antidelito.java

--- src/gestorAplicacion/carcel/Antidelito.java
+++ src/gestorAplicacion/carcel/Antidelito.java
@@ -6,4 +6,40 @@ public class Antidelito {
 	private int codigo;
     private String nombre;
     private long rebajaCondena; /*meses*/
+    
+    public Antidelito(int codigo, String nombre, long rebajaCondena) {
+    	this.codigo = codigo;
+    	this.nombre = nombre;
+    	this.rebajaCondena = rebajaCondena;
+    	antidelitos.put(this.codigo, this);
+    }
+
+	public static Hashtable<Integer, Antidelito> getAntidelitos() {
+		return antidelitos;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public long getRebajaCondena() {
+		return rebajaCondena;
+	}
+
+	public void setRebajaCondena(long rebajaCondena) {
+		this.rebajaCondena = rebajaCondena;
+	}
+    
 }
