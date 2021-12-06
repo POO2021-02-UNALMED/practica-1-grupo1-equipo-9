6391d2ae64a1260d9023fcb8f31f22629338d8df
jumartineze <jumartineze@unal.edu.co>
2021-12-06 16:10:29 -0500

    constructores Prisionero

--- src/gestorAplicacion/carcel/Prisionero.java
+++ src/gestorAplicacion/carcel/Prisionero.java
@@ -2,7 +2,9 @@ package gestorAplicacion.carcel;
 import java.util.Hashtable;
 import java.time.LocalDate;
 
-public class Prisionero {
+import gestorAplicacion.apuestas.Apostador;
+
+public class Prisionero extends Apostador{
     private gestorAplicacion.carcel.genero genero;
     private Celda celda;
     private LocalDate inicioCondena;
@@ -12,16 +14,36 @@ public class Prisionero {
     
     private static Hashtable<Integer, Celda> celdas;
     
+    public Prisionero(int identificacion, String nombre, Celda celda, Hashtable<Integer, Delito> delitos) {
+    	this(identificacion,nombre,0,celda,delitos);
+    }
+    
+    public Prisionero(int identificacion, String nombre, int saldo, Celda celda, Hashtable<Integer, Delito> delitos) {
+    	super(identificacion, nombre, saldo);
+    	this.celda = celda;
+    	this.delitos = delitos;
+    	// calcular inicioCondena y finCondena
+    }
+    
     public void agregarDelito(Delito delito) {
     	
     }
+    
     public void agregarAntidelito(Antidelito antidelito) {
     	
     }
+    
     private void incrementarCondena(Long meses) { //reciben como argumento el tiempoCondena de Delito
-    	// usar el método LocalDate.plusMonths() y devuelve una copia
+    	// usar el mï¿½todo LocalDate.plusMonths() y devuelve una copia
     }
+    
     private void dismnuirCondena(Long meses) { //reciben como argumento el rebajarCondena de Antidelito
     	
-    }  
+    }
+    
+    @Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
