e9ff44543973d550cf05ccbcfc108c1ab04df10d
Valentina Gomez Valencia <begomezv@unal.edu.co>
2021-12-06 16:42:32 -0500

    Update Celda.java

--- src/gestorAplicacion/carcel/Celda.java
+++ src/gestorAplicacion/carcel/Celda.java
@@ -4,13 +4,23 @@ import java.util.Hashtable;
 public class Celda {
 	private int numero;
     private gestorAplicacion.carcel.genero genero;
-    private int largo;
-    private int ancho;
-    private int capMax;
+    private final int largo;
+    private final int ancho;
+    private final int capMax;
     private String tipo;
     private Hashtable<Integer, Prisionero> prisioneros;
     
     private static Hashtable<Integer, Celda> celdas;
+    
+    public Celda(int numero, gestorAplicacion.carcel.genero genero, int largo, int ancho,int capMax, String tipo) {
+    	this.numero=numero;
+    	this.genero=genero;
+    	this.largo=largo;
+    	this.ancho=ancho;
+    	this.capMax=capMax;
+    	this.tipo=tipo;
+    	
+    }
     
     public void extraerPrisionero(Prisionero prisionero) {
 //    	Hace prisionero.celda = null y prisionero.celda.remove(prisionero)
