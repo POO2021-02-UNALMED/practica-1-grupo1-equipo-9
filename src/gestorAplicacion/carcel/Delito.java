da41382f40f9942bd857a37a583fffad8afc7f80
jumartineze <jumartineze@unal.edu.co>
2021-12-06 17:04:34 -0500

    Constructor Delito

--- src/gestorAplicacion/carcel/Delito.java
+++ src/gestorAplicacion/carcel/Delito.java
@@ -5,7 +5,17 @@ import java.util.Hashtable;
 public class Delito {
 	private static Hashtable<Integer,Delito> delitos= new Hashtable<Integer,Delito>();
     private int codigo;
-    private String categoria;
-    private String nivel;
+    private String nombre;
+    private String descripcion;
+    private int nivel;
     private long tiempoCondena; /*meses*/
+    
+    public Delito(int codigo, String nombre, String descripcion, int nivel, long tiempoCondena) {
+    	this.codigo = codigo;
+    	this.nombre = nombre;
+    	this.descripcion = descripcion;
+    	this.nivel = nivel;
+    	this.tiempoCondena = tiempoCondena;
+    	delitos.put(this.codigo, this);
+    }
 }
