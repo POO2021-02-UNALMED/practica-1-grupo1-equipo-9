2af24a37579200b86a44c85ee1444672fa7a7244
Alejandro Salazar Mejía <alejosalazarmejia.2000@gmail.com>
2021-12-08 13:20:27 -0500

    Update Carcel.java

--- src/uiMain/Carcel.java
+++ src/uiMain/Carcel.java
@@ -1,20 +1,30 @@
 package uiMain;
 
+import java.time.LocalDate;
+
 import gestorAplicacion.carcel.Celda;
 import gestorAplicacion.carcel.Prisionero;
 import gestorAplicacion.carcel.genero;
 
 public class Carcel {
+	
+	static {
+//		crearDelitos
+		
+//		crearCeldas
+		
+//		crearPrisioneros
+		
+//		crearGuardianes
+		
+//		crearAntidelitos
+		
+//		crearPeleas
+		
+	}
 
     public static void main(String[] args) {
         // TODO code application logic here
-		/*
-		 * Celda c = new Celda(0, genero.MASCULINO, 0, 0, 0); Prisionero p = new
-		 * Prisionero(1, "Alejo", genero.MASCULINO, c, null);
-		 * System.out.println(p.getSaldo());
-		 */
-    	Integer i = 12;
-    	System.out.println(i);
+		
     }
-    
 }
