b64af4f27111cab83725670f2345da046c22e4a2
Valentina Gomez Valencia <begomezv@unal.edu.co>
2021-12-16 14:32:39 -0500

    Update genero.java

--- src/gestorAplicacion/carcel/genero.java
+++ src/gestorAplicacion/carcel/genero.java
@@ -1,3 +1,8 @@
+/*
+ * La finalidad de esta clase es mantener la consistencia:
+ *Que el genero de los prisioneros coincidan con las celdas 
+ *y las peleas
+ */
 package gestorAplicacion.carcel;
 
 public enum genero {
