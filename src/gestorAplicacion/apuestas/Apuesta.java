6df9e7888de70adfa9ff587df18ecdfce37f2272
Alejandro Salazar Mejía <alejosalazarmejia.2000@gmail.com>
2021-12-16 16:06:25 -0500

    Update Apuesta.java

--- src/gestorAplicacion/apuestas/Apuesta.java
+++ src/gestorAplicacion/apuestas/Apuesta.java
@@ -76,7 +76,7 @@ public class Apuesta implements Serializable{
 	 * - Luego se les agrega al saldo de los ganadores una cantidad proporcional a su apuesta en relacion al 
 	 *   monto total apostado.
 	 * 
-	 * Nota: Se hace uso del resultado que tuvo la pela para definir a los ganadores y tambien del casteo 
+	 * Nota: Se hace uso del resultado que tuvo la pelea para definir a los ganadores y tambien del casteo 
 	 * explicito.
 	 */
     public void resolverApuesta() {
@@ -86,6 +86,10 @@ public class Apuesta implements Serializable{
     	double totalGanadores = 0;
     	
     	for (Object[] objects : apostadores) {
+			/* Este for se encarga de calcular el monto total de la apuesta
+			 * y lo guarda en montoTotal.
+			 * Tambien calcula el total apostador POR LOS GANADORES, y 
+			 * lo guarda en totalGanadores*/
     		Prisionero prisionero = (Prisionero) objects[1];
     		double apuesta = (Integer) objects[2];
     		montoTotal += apuesta;
@@ -96,10 +100,16 @@ public class Apuesta implements Serializable{
     	
     	for (Object[] objects : apostadores) {
 
+//    		Castea cada uno de los componentes de las "tuplas" en su respectivo tipo original
     		Apostador apostador = (Apostador) objects[0];
     		Prisionero prisionero = (Prisionero) objects[1];
     		double apuesta = (Integer) objects[2];
     		
+    		/* Este if-else se encarga de revisar si cada apostador ganó. Si es así, entonces
+    		 * se calcula la ganancia con base en lo que apostó y se agrega a su sueldo.
+    		 * Tambien se agrega a this.estadistica el resultado de este apostador, es decir,
+    		 * su ID, cuanto ganó y su saldo actual.
+    		 * En caso de que halla perdido, se pone cuanto perdio. */
     		if (pelea.getGanador().equals(prisionero)) {
     			double k = apuesta/totalGanadores;
     			double ganancia = k*montoTotal;
