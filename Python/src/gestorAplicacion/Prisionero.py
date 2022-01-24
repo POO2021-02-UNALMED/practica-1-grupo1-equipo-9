627ea2b6a6630b4a6063797285edc36acf516d4c
Valentina Gomez Valencia <begomezv@unal.edu.co>
2022-01-23 20:42:21 -0500

    Update Prisionero.py

--- Python/src/gestorAplicacion/Prisionero.py
+++ Python/src/gestorAplicacion/Prisionero.py
@@ -1,46 +1,51 @@
-from apostador import Apostador;
+from apostador import Apostador
+from datetime import date
 
 class Prisionero(Apostador):
+    _delitos = {}
+    _antidelitos = {}
+    _prisioneros = {}
     prisionerosMASCULINOS = []
     prisionerosFEMENINOS = []
 
 
-    def __init__(self, identificacion, nombre, saldo, genero, celda, delitos):
+    def __init__(self, identificacion, nombre, saldo, _genero, _celda, _delitos):
         Apostador.__init__(self, identificacion, nombre, saldo)
-        self.genero = genero
-        self.celda = celda
-        self.delitos = delitos
+        self._genero = _genero
+        self._inicioCondena = date.today()
+        self._celda = _celda
+        self._delitos = _delitos
 
-        if self.genero=="MASCULINO":
+        if self._genero=="MASCULINO":
             Prisionero.prisionerosMASCULINOS.append(self.identificacion)
 
-        elif self.genero=="FEMENINO":
+        elif self._genero=="FEMENINO":
             Prisionero.prisionerosFEMENINOS.append(self.identificacion) 
 
     def getGenero(self):
-        return self.genero
-    def setGenero(self, genero):
-        self.genero = genero
+        return self._genero
+    def setGenero(self, _genero):
+        self._genero = _genero
 
     def getCelda(self):
-        return self.celda
-    def setCelda(self, celda):
-        self.celda = celda
+        return self._celda
+    def setCelda(self, _celda):
+        self._celda = _celda
 
-    '''def getInicioCondena(self):
-        return self.inicioCondena
-    def setInicioCondena(self, inicioCondena):
-        self.inicioCondena = inicioCondena 
+    def getInicioCondena(self):
+        return self._inicioCondena
+    def setInicioCondena(self, _inicioCondena):
+        self._inicioCondena = _inicioCondena 
 
     def getFinCondena(self):
-        return self.FinCondena
-    def setFinCondena(self, finCondena):
-        self.finCondena = finCondena  '''      
+        return self._finCondena
+    def setFinCondena(self, _finCondena):
+        self._finCondena = _finCondena      
 
     def getDelitos(self):
-        return self.delitos
-    def setDelitos(self, delitos):
-        self.delitos = delitos    
+        return self._delitos
+    def setDelitos(self, _delitos):
+        self._delitos = _delitos    
 
     def getAntidelitos(self):
         return self.antidelitos
@@ -48,6 +53,6 @@ class Prisionero(Apostador):
         self.antidelitos = antidelitos  
 
     def getPrisioneros(self):
-        return self.prisioneros
-    def setPrisioneros(self, prisioneros):
-        self.prisioneros = prisioneros          
+        return self._prisioneros
+    def setPrisioneros(self, _prisioneros):
+        self._prisioneros = _prisioneros          
