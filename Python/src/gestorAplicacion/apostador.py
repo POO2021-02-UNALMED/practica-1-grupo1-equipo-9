a1ab773389246757ee8fcef26da2c41bfd62dad6
jumartineze <jumartineze@unal.edu.co>
2022-01-23 22:41:19 -0500

    clases

--- Python/src/gestorAplicacion/apostador.py
+++ Python/src/gestorAplicacion/apostador.py
@@ -1,26 +1,26 @@
 class Apostador:
-    def __init__(self, identificacion, nombre, saldo):
+    def __init__(self, identificacion: int, nombre: str, saldo: int):
         self.identificacion = identificacion
         self.nombre = nombre
         self.saldo = saldo
 
-    def aumentarSaldo(self, dinero):
+    def aumentarSaldo(self, dinero: int):
         self.saldo += dinero
 
-    def reducirSaldo(self, dinero):
+    def reducirSaldo(self, dinero: int):
         self.saldo -= dinero
 
     def getIdentificacion(self):
         return self.identificacion
-    def setIdentificacion(self, identificacion):
+    def setIdentificacion(self, identificacion: int):
         self.identificacion = identificacion
 
     def getNombre(self):
         return self.nombre
-    def setNombre(self, nombre):
+    def setNombre(self, nombre: str):
         self.nombre = nombre
 
     def getSaldo(self):
         return self.saldo
-    def setSaldo(self, saldo):
+    def setSaldo(self, saldo: int):
         self.saldo = saldo
