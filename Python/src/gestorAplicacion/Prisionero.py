345398821b4a57fc2334d8a4872ccfa4c003fd1f
Valentina Gomez Valencia <begomezv@unal.edu.co>
2022-01-23 20:12:32 -0500

    Update Prisionero.py

--- Python/src/gestorAplicacion/Prisionero.py
+++ Python/src/gestorAplicacion/Prisionero.py
@@ -1,6 +1,6 @@
-import Apostador;
+from apostador import Apostador;
 
-class prisionero:
+class Prisionero(Apostador):
     def __init__(self, identificacion, nombre, saldo, genero, celda, delitos):
         self.identificacion = identificacion
         self.nombre = nombre
@@ -8,3 +8,44 @@ class prisionero:
         self.genero = genero
         self.celda = celda
         self.delitos = delitos
+
+        if self.genero=="MASCULINO":
+            prisionerosMASCULINOS.append(identificacion)
+
+        elif self.genero=="FEMENINO":
+            prisionerosFEMENINOS.append(identificacion) 
+
+    def getGenero(self):
+        return self.genero
+    def setGenero(self, genero):
+        self.genero = genero
+
+    def getCelda(self):
+        return self.celda
+    def setCelda(self, celda):
+        self.celda = celda
+
+    '''def getInicioCondena(self):
+        return self.inicioCondena
+    def setInicioCondena(self, inicioCondena):
+        self.inicioCondena = inicioCondena 
+
+    def getFinCondena(self):
+        return self.FinCondena
+    def setFinCondena(self, finCondena):
+        self.finCondena = finCondena  '''      
+
+    def getDelitos(self):
+        return self.delitos
+    def setDelitos(self, delitos):
+        self.delitos = delitos    
+
+    def getAntiDelitos(self):
+        return self.antiDelitos
+    def setAntiDelitos(self, antiDelitos):
+        self.antiDelitos = antiDelitos  
+
+    def getPrisioneros(self):
+        return self.prisioneros
+    def setPrisioneros(self, prisioneros):
+        self.prisioneros = prisioneros          
