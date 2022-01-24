76860031cc46a151ed3607f4d46be6332e2d2803
jumartineze <jumartineze@unal.edu.co>
2022-01-23 19:58:02 -0500

    apostador

--- Python/src/gestorAplicacion/Apostador.py
+++ Python/src/gestorAplicacion/Apostador.py
@@ -1,2 +1,26 @@
 class Apostador:
-    pass
+    def __init__(self, identificacion, nombre, saldo):
+        self.identificacion = identificacion
+        self.nombre = nombre
+        self.saldo = saldo
+
+    def aumentarSaldo(self, dinero):
+        self.saldo += dinero
+
+    def reducirSaldo(self, dinero):
+        self.saldo -= dinero
+
+    def getIdentificacion(self):
+        return self.identificacion
+    def setIdentificacion(self, identificacion):
+        self.identificacion = identificacion
+
+    def getNombre(self):
+        return self.nombre
+    def setNombre(self, nombre):
+        self.nombre = nombre
+
+    def getSaldo(self):
+        return self.saldo
+    def setSaldo(self, saldo):
+        self.saldo = saldo
