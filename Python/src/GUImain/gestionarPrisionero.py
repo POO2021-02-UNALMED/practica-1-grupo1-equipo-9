bd74b3c59c851b716890acacf607dcb8138bfa31
Valentina Gomez Valencia <begomezv@unal.edu.co>
2022-02-01 17:59:39 -0500

    Update gestionarPrisionero.py

--- Python/src/GUImain/gestionarPrisionero.py
+++ Python/src/GUImain/gestionarPrisionero.py
@@ -1,21 +1,21 @@
-from cgitb import text
-from importlib.metadata import entry_points
-from struct import pack
 from textwrap import fill
-from tkinter import *
-from tkinter import font
+import tkinter as tk
+from tkinter import Canvas, Label, ttk
 from tkinter import messagebox
-
-from gestorAplicacion.prisionero import Prisionero
-from .utils.fieldFrame import FieldFrame
+from tkinter import font
+from turtle import width
 
 from numpy import pad
+from gestorAplicacion.delito import Delito
+from gestorAplicacion.antidelito import Antidelito
+from gestorAplicacion.prisionero import Prisionero
+from .utils.fieldFrame import FieldFrame
 from .utils.menuBar import MenuBar
 
 
-class GestionarPrisionero(Toplevel):
+class GestionarPrisionero(tk.Toplevel):
 
-    def __init__(self, window: Tk):
+    def __init__(self, window: tk.Tk):
         super().__init__(window)
         self.MASTER = window
 
@@ -50,29 +50,29 @@ class GestionarPrisionero(Toplevel):
         menubar.add_menu_options("Ayuda", menuAyuda)
 
     def frmInicial(self):
-        frmBase = Frame(self)
+        frmBase = tk.Frame(self)
 
-        frm_titulo_proceso = Frame(frmBase)
-        frm_titulo_proceso.pack(side=TOP, fill=X, padx=10 ,pady= 10)
+        frm_titulo_proceso = tk.Frame(frmBase)
+        frm_titulo_proceso.pack(side=tk.TOP, fill=tk.X, padx=10 ,pady= 10)
 
-        lbl_titulo_proceso = Label(frm_titulo_proceso, text="Gestión de Prisioneros", font=('Arial', 15))
+        lbl_titulo_proceso = tk.Label(frm_titulo_proceso, text="Gestión de Prisioneros", font=('Arial', 15))
         lbl_titulo_proceso.pack()
         
-        frm_descripcion_proceso = Frame(frmBase, borderwidth=2, relief="solid")
-        frm_descripcion_proceso.pack(fill=X, padx=10 ,pady= 10)
+        frm_descripcion_proceso = tk.Frame(frmBase, borderwidth=2, relief="solid")
+        frm_descripcion_proceso.pack(fill=tk.X, padx=10 ,pady= 10)
 
-        lbl_descripcion_proceso = Label(frm_descripcion_proceso,
+        lbl_descripcion_proceso = tk.Label(frm_descripcion_proceso,
         text= """En esta ventana tendrá la posibilidad de gestionar todos los aspectos
         relacionados con los prisioneros recluidos en las instalaciones de la Cárcel Apuestera.""", 
         font=('Arial', 10))
         lbl_descripcion_proceso.pack(pady= 10)
 
-        frm_formulario = Frame(frmBase, borderwidth=1, relief="solid")
-        frm_formulario.pack(fill=X, pady= 10)
-        lbl_formulario = Label(frm_formulario,
+        frm_formulario = tk.Frame(frmBase, borderwidth=1, relief="solid")
+        frm_formulario.pack(fill=tk.X, pady= 10)
+        lbl_formulario = tk.Label(frm_formulario,
         text= """En el menú 'Procesos y Consultas' se encuentran las funcionalidades ofrecidas""", 
         font=('Arial', 10))
-        lbl_formulario.pack(fill=X, padx=10 ,pady= 10)
+        lbl_formulario.pack(fill=tk.X, padx=10 ,pady= 10)
 
 
         self.currFrame = frmBase
@@ -81,158 +81,185 @@ class GestionarPrisionero(Toplevel):
     def ingresarPrisionero(self):
         self.currFrame.pack_forget()
 
-        frm_ingresarPrisionero=Frame(self)
+        frm_ingresarPrisionero=tk.Frame(self)
 
         frm_ingresar=FieldFrame(frm_ingresarPrisionero, "Criterios", ["Identificación", "Nombre", "Saldo", "Género", "Celda", "Delitos"], 
-         "Valores", [str(len(Prisionero.getPrisioneros())+1), None, None, None, None, None], [DISABLED, NORMAL, NORMAL, NORMAL, NORMAL, NORMAL], "Ingresar Prisionero", 
-         "Ingrese los datos solicitados para registrar el Prisionero")
+         "Valores", [str(len(Prisionero.getPrisioneros())+1), None, None, None, None, None], [tk.DISABLED, tk.NORMAL, tk.NORMAL, tk.NORMAL, tk.NORMAL, tk.NORMAL], 
+         "Ingresar Prisionero", "Ingrese los datos solicitados para registrar el prisionero")
 
         def registro():
             pass
 
         frm_ingresar.set_command_btn_aceptar(registro)
-        frm_ingresar.pack(fill=BOTH, expand=TRUE)
+        frm_ingresar.pack(fill=tk.BOTH, expand=True)
         self.currFrame=frm_ingresarPrisionero
         frm_ingresarPrisionero.pack()
-
-        
+      
     def borrarPrisionero(self):
         self.currFrame.pack_forget()
 
-        frm_borrarPrisionero=Frame(self)
+        frm_borrarPrisionero=tk.Frame(self)
 
-        frm_titulo_proceso=Frame(frm_borrarPrisionero)
-        frm_titulo_proceso.pack(side=TOP, fill=X, padx=10, pady=10)
+        frm_titulo_proceso=tk.Frame(frm_borrarPrisionero)
+        frm_titulo_proceso.pack(side=tk.TOP, fill=tk.X, padx=10, pady=10)
 
-        lbl_titulo_proceso=Label(frm_titulo_proceso, text="Borrar Prisionero", font=('Arial', 15))
+        lbl_titulo_proceso=tk.Label(frm_titulo_proceso, text="Borrar Prisionero", font=('Arial', 15))
         lbl_titulo_proceso.pack()
 
-        frm_descripcion_proceso=Frame(frm_borrarPrisionero, borderwidth=2, relief="solid")
-        frm_descripcion_proceso.pack(fill=X, padx=10, pady=10)
+        frm_descripcion_proceso=tk.Frame(frm_borrarPrisionero, borderwidth=2, relief="solid")
+        frm_descripcion_proceso.pack(fill=tk.X, padx=10, pady=10)
 
-        lbl_descripcion_proceso = Label(frm_descripcion_proceso, text="Ingrese la identificación del Prisionero", font=('Arial', 10))
-        lbl_descripcion_proceso.pack(fill=X, padx=10, pady=10)
+        lbl_descripcion_proceso = tk.Label(frm_descripcion_proceso, text="Seleccione la identificación del Prisionero que desea eliminar", font=('Arial', 10))
+        lbl_descripcion_proceso.pack(fill=tk.X, padx=10, pady=10)
 
+        frm_borrar=tk.Frame(frm_borrarPrisionero, borderwidth=1, relief="solid")
+        prisioneros=[k for (k,v) in Prisionero.getPrisioneros().items()]
 
-        frm_dato=Frame(frm_borrarPrisionero, borderwidth=1, relief="solid")
+        tk.Label(frm_borrar, text="Identificación: ").grid(column=0, row=1, padx=15, pady=5)
+        cbox_identificacion=ttk.Combobox(frm_borrar, values=prisioneros, justify=tk.CENTER, state="readonly")
+        cbox_identificacion.grid(column=1,row=1,padx=15,pady=5)
 
-        Label(frm_dato, text="Identificación: ").grid(column=0, row=0, padx=15, pady=5)
-        entry_Identificacion=Entry(frm_dato, justify=CENTER)
-        entry_Identificacion.grid(column=1, row=0, padx=15,pady=5)
-
-        def borrar():
-            entry_Identificacion.delete(0,END)
-            entry_Identificacion.insert(0,"")
 
         def func_borrarPrisionero():
-            messagebox.askyesno(message="¿Está seguro que desea eliminar el Prisionero?")
+            messagebox.askyesno(message="¿Está seguro que desea eliminar el prisionero?")
 
             if(True):
                 pass
             else:
-                pass    
+                pass   
+
+        def cancelar():
+            pass     
 
         fuente="Helvetica 10 bold"
-        btn_aceptar = Button(frm_dato, text="Aceptar", font=fuente, command= func_borrarPrisionero)
+        btn_aceptar = tk.Button(frm_borrar, text="Aceptar", font=fuente, command= func_borrarPrisionero)
         btn_aceptar.grid(column=0, row=5, padx=15, pady=15)
-        btn_borrar = Button(frm_dato, text="Borrar", font=fuente, command= borrar)
-        btn_borrar.grid(column=1, row=5, padx=15, pady=15)
+        btn_cancelar = tk.Button(frm_borrar, text="Cancelar", font=fuente, command= cancelar)
+        btn_cancelar.grid(column=1, row=5, padx=15, pady=15)
 
-        frm_dato.pack(expand=True, padx=30)
+        frm_borrar.pack(expand=True, padx=30)
         self.currFrame = frm_borrarPrisionero
         frm_borrarPrisionero.pack()
 
     def agregarDelito(self):
         self.currFrame.pack_forget()
 
-        frm_agregarDelito=Frame(self)
+        frm_agregarDelito=tk.Frame(self)
 
-        frm_titulo_proceso=Frame(frm_agregarDelito)
-        frm_titulo_proceso.pack(side=TOP, fill=X, padx=10, pady=10)
+        frm_titulo_proceso=tk.Frame(frm_agregarDelito)
+        frm_titulo_proceso.pack(side=tk.TOP, fill=tk.X, padx=10, pady=10)
 
-        lbl_titulo_proceso=Label(frm_titulo_proceso, text="Agregar Delito", font=('Arial', 15))
+        lbl_titulo_proceso=tk.Label(frm_titulo_proceso, text="Agregar Delito", font=('Arial', 15))
         lbl_titulo_proceso.pack()
 
-        frm_descripcion_proceso=Frame(frm_agregarDelito, borderwidth=2, relief="solid")
-        frm_descripcion_proceso.pack(fill=X, padx=10, pady=10)
+        frm_descripcion_proceso=tk.Frame(frm_agregarDelito, borderwidth=2, relief="solid")
+        frm_descripcion_proceso.pack(fill=tk.X, padx=10, pady=10)
 
-        lbl_descripcion_proceso = Label(frm_descripcion_proceso, text="Ingrese el Delito que desea agregar al Prisionero", font=('Arial', 10))
-        lbl_descripcion_proceso.pack(fill=X, padx=10, pady=10)
+        lbl_descripcion_proceso = tk.Label(frm_descripcion_proceso, text="Seleccione el código del delito que desea agregar", font=('Arial', 10))
+        lbl_descripcion_proceso.pack(fill=tk.X, padx=10, pady=10)
 
+        frm_agregar=tk.Frame(frm_agregarDelito, borderwidth=1, relief="solid")
+        delitos=[k for (k,v) in Delito.getDelitos().items()]
 
-        frm_dato=Frame(frm_agregarDelito, borderwidth=1, relief="solid")
-
-        Label(frm_dato, text="Nombre: ").grid(column=0, row=0, padx=15, pady=5)
-        entry_Nombre=Entry(frm_dato, justify=CENTER)
-        entry_Nombre.grid(column=1, row=0, padx=15,pady=5)
-
-        def borrar():
-            entry_Nombre.delete(0,END)
-            entry_Nombre.insert(0,"")
+        tk.Label(frm_agregar, text="Código: ").grid(column=0, row=1, padx=15, pady=5)
+        cbox_identificacion=ttk.Combobox(frm_agregar, values=delitos, justify=tk.CENTER, state="readonly")
+        cbox_identificacion.grid(column=1,row=1,padx=15,pady=5)  
 
         def func_agregarDelito():
             pass
+        def cancelar():
+            pass     
 
         fuente="Helvetica 10 bold"
-        btn_aceptar = Button(frm_dato, text="Aceptar", font=fuente, command= func_agregarDelito)
+        btn_aceptar = tk.Button(frm_agregar, text="Aceptar", font=fuente, command= func_agregarDelito)
         btn_aceptar.grid(column=0, row=5, padx=15, pady=15)
-        btn_borrar = Button(frm_dato, text="Borrar", font=fuente, command= borrar)
-        btn_borrar.grid(column=1, row=5, padx=15, pady=15)
+        btn_cancelar = tk.Button(frm_agregar, text="Cancelar", font=fuente, command= cancelar)
+        btn_cancelar.grid(column=1, row=5, padx=15, pady=15)
 
-        frm_dato.pack(expand=True, padx=30)
-        self.currFrame = frm_agregarDelito 
+        frm_agregar.pack(expand=True, padx=30)
+        self.currFrame = frm_agregarDelito
         frm_agregarDelito.pack()
 
     def agregarAntidelito(self):
         self.currFrame.pack_forget()
 
-        frm_agregarAntidelito=Frame(self)
+        frm_agregarAntidelito=tk.Frame(self)
 
-        frm_titulo_proceso=Frame(frm_agregarAntidelito)
-        frm_titulo_proceso.pack(side=TOP, fill=X, padx=10, pady=10)
+        frm_titulo_proceso=tk.Frame(frm_agregarAntidelito)
+        frm_titulo_proceso.pack(side=tk.TOP, fill=tk.X, padx=10, pady=10)
 
-        lbl_titulo_proceso=Label(frm_titulo_proceso, text="Agregar Antidelito", font=('Arial', 15))
+        lbl_titulo_proceso=tk.Label(frm_titulo_proceso, text="Agregar Antidelito", font=('Arial', 15))
         lbl_titulo_proceso.pack()
 
-        frm_descripcion_proceso=Frame(frm_agregarAntidelito, borderwidth=2, relief="solid")
-        frm_descripcion_proceso.pack(fill=X, padx=10, pady=10)
+        frm_descripcion_proceso=tk.Frame(frm_agregarAntidelito, borderwidth=2, relief="solid")
+        frm_descripcion_proceso.pack(fill=tk.X, padx=10, pady=10)
 
-        lbl_descripcion_proceso = Label(frm_descripcion_proceso, text="Ingrese el Antidelito que desea agregar al Prisionero", font=('Arial', 10))
-        lbl_descripcion_proceso.pack(fill=X, padx=10, pady=10)
+        lbl_descripcion_proceso = tk.Label(frm_descripcion_proceso, text="Seleccione el código del antidelito que desea agregar", font=('Arial', 10))
+        lbl_descripcion_proceso.pack(fill=tk.X, padx=10, pady=10)
 
-        frm_dato=Frame(frm_agregarAntidelito, borderwidth=1, relief="solid")
+        frm_agregar=tk.Frame(frm_agregarAntidelito, borderwidth=1, relief="solid")
+        antidelitos=[k for (k,v) in Antidelito.getAntidelitos().items()]
 
-        Label(frm_dato, text="Nombre: ").grid(column=0, row=0, padx=15, pady=5)
-        entry_Nombre=Entry(frm_dato, justify=CENTER)
-        entry_Nombre.grid(column=1, row=0, padx=15,pady=5)
-
-        def borrar():
-            entry_Nombre.delete(0,END)
-            entry_Nombre.insert(0,"")
+        tk.Label(frm_agregar, text="Código: ").grid(column=0, row=1, padx=15, pady=5)
+        cbox_identificacion=ttk.Combobox(frm_agregar, values=antidelitos, justify=tk.CENTER, state="readonly")
+        cbox_identificacion.grid(column=1,row=1,padx=15,pady=5)  
 
         def func_agregarAntidelito():
             pass
+        def cancelar():
+            pass     
 
         fuente="Helvetica 10 bold"
-        btn_aceptar = Button(frm_dato, text="Aceptar", font=fuente, command= func_agregarAntidelito)
+        btn_aceptar = tk.Button(frm_agregar, text="Aceptar", font=fuente, command= func_agregarAntidelito)
         btn_aceptar.grid(column=0, row=5, padx=15, pady=15)
-        btn_borrar = Button(frm_dato, text="Borrar", font=fuente, command= borrar)
-        btn_borrar.grid(column=1, row=5, padx=15, pady=15)
+        btn_cancelar = tk.Button(frm_agregar, text="Cancelar", font=fuente, command= cancelar)
+        btn_cancelar.grid(column=1, row=5, padx=15, pady=15)
 
-        frm_dato.pack(expand=True, padx=30)
-        self.currFrame = frm_agregarAntidelito 
+        frm_agregar.pack(expand=True, padx=30)
+        self.currFrame = frm_agregarAntidelito
         frm_agregarAntidelito.pack()
 
     def listarPrisionero(self):
         self.currFrame.pack_forget()
 
-        frm_listaPrisioneros=Frame(self)
+        frm_listaPrisioneros=tk.Frame(self)
 
-        frm_titulo_proceso=Frame(frm_listaPrisioneros)
-        frm_titulo_proceso.pack(side=TOP, fill=X, padx=10,pady=10)
+        frm_titulo_proceso=tk.Frame(frm_listaPrisioneros)
+        frm_titulo_proceso.pack(side=tk.TOP, fill=tk.X, padx=10,pady=10)
 
-        lbl_titulo_proceso=Label(frm_titulo_proceso, text="Lista de Prisioneros", font=('Arial', 13, 'bold'))
+        lbl_titulo_proceso=tk.Label(frm_titulo_proceso, text="Lista de Prisioneros", font=('Arial', 13, 'bold'))
         lbl_titulo_proceso.pack()
+
+        frm_descripcion_proceso=tk.Frame(frm_listaPrisioneros, borderwidth=2, relief="solid", padx=10)
+        frm_descripcion_proceso.pack(fill=tk.X, padx=10, pady=10)
+
+        lbl_descipcion_proceso=tk.Label(frm_descripcion_proceso, text="A continuación podrá ver todos los prisioneros que se encuentran recluidos en la cárcel",
+        font=('Arial',10))
+        lbl_descipcion_proceso.pack(pady=10)
+
+        container=ttk.Frame(frm_listaPrisioneros, borderwidth=1, relief="solid")
+        colores=["gray70", "white"]
+        canvas=Canvas(container, width=550, height=250)
+        scrollbar=ttk.Scrollbar(container, orient="vertical", command=canvas.yview)
+        scrollbar_frame=ttk.Frame(canvas)
+
+        scrollbar_frame.bind(
+            "<Configure>",
+            lambda e: canvas.configure(
+                scrollregion=canvas.bbox("all")
+            )
+        )
+        canvas.create_window((0,0), window=scrollbar_frame, anchor="nw")
+        canvas.configure(yscrollcommand=scrollbar.set)
+
+        for pri in Prisionero.getPrisioneros().values():
+            currLabel=Label(scrollbar_frame, background=colores[0], text=pri, width=80)
+            colores.append(colores.pop(0))
+            currLabel.pack(fill=tk.X)
+
+        container.pack()
+        canvas.pack(side="left", fill="both", expand=True)
+        scrollbar.pack(side="right", fill="y")
 
         self.currFrame=frm_listaPrisioneros
         frm_listaPrisioneros.pack()
